636eb6b15d9110ad57891ac9814d6e0938a51201
wishizerg <>
2013-11-04 18:48:51 +0100

    make a start

---
+++ eu.4clown.starmade-universe-tool/src/eu/pkg4clown/starmade/universe/tool/StarmadeUniverseTool.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eu.pkg4clown.starmade.universe.tool;
+
+/**
+ *
+ * @author nf
+ */
+public class StarmadeUniverseTool {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
