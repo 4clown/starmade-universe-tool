737a5036d52166ae89ea7b4fe5b1ffc57a69240d
Daniel <d.rautenberg1@googlemail.com>
2013-11-05 15:47:12 +0100

    Nicht funtionierender BufferedReader Test.

--- eu.4clown.starmade-universe-tool/src/eu/pkg4clown/starmade/universe/tool/Config.java
+++ eu.4clown.starmade-universe-tool/src/eu/pkg4clown/starmade/universe/tool/Config.java
@@ -3,13 +3,22 @@
  * and open the template in the editor.
  */
 package eu.pkg4clown.starmade.universe.tool;
-
+import java.io.*;
 /**
  *
  * @author nf
  */
-public class Config {
+public class Config
+{
     
-    //blubber
+    try {
+            BufferedReader in = new BufferedReader(new FileReader("Config.txt"));
+            String zeile = null;
+            while ((zeile = in.readLine()) != null) {
+                System.out.pintln("Geleene Zeile: " + zeile);
+            }
+    } catch (IOException e) {
+        e.printStackTrace();
+    }
     
 }
