5eaf2dbc480909733f95164e2e04932b598872e7
Daniel <d.rautenberg1@googlemail.com>
2013-11-06 15:26:50 +0100

    Auslesen von Werten aus bestimmten Zeilen

--- eu.4clown.starmade-universe-tool/src/eu/pkg4clown/starmade/universe/tool/Config.java
+++ eu.4clown.starmade-universe-tool/src/eu/pkg4clown/starmade/universe/tool/Config.java
@@ -9,26 +9,31 @@ import java.io.*;
 class Config {
 
     private String test;
-
+    private String command;
+    
     public Config(String path) {
         try {
             IOException ex;
             FileReader fr = new FileReader(path);
             BufferedReader br = new BufferedReader(fr);
             this.test = br.readLine();
-
+            
             while (test != null) {
                 test = br.readLine();
-
                 test += test;
-
             }
-
+        
         } catch (IOException ex) {
             ex.getMessage();
             System.out.println(ex);
         }
-
         System.out.println(test);
+     }
+        
+    public String parameter(String command) {
+       
+        int start = test.indexOf(command) + command.length();
+        int end = test.indexOf("\n",start);
+        return test.substring(start,end);
     }
 }
