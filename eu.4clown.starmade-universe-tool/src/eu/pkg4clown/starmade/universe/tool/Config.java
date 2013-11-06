d37095637129818734b906bc9ffd3b4ca6ae8414
Daniel <d.rautenberg1@googlemail.com>
2013-11-06 11:55:29 +0100

    IOException sollte nun wirklich von der Klasse Config nicht weitergegeben werden

--- eu.4clown.starmade-universe-tool/src/eu/pkg4clown/starmade/universe/tool/Config.java
+++ eu.4clown.starmade-universe-tool/src/eu/pkg4clown/starmade/universe/tool/Config.java
@@ -3,36 +3,26 @@
  * and open the template in the editor.
  */
 package eu.pkg4clown.starmade.universe.tool;
+
 import java.io.*;
 
-public class Config
-{
-           public Config() throws IOException
-           {
-               IOException ex;
-               FileReader fr = new FileReader("config.txt");
-               BufferedReader br = new BufferedReader(fr);
-               
-               String test = br.readLine();
-               
-               while ( test != null )
-               {
-                   test = br.readLine();
-                   System.out.println(test);
-                   
-               }
-                              
-               br.close();
-               
-               /*catch (IOException ex)
-               {
-                       ex.getMessage();
-               }
-               */
-           }
-           
-           catch IOException ex)
-           {
-               ex.getMessage();
-           }
+public class Config {
+
+    public Config() throws IOException
+    {
+        try {
+            IOException ex;
+            FileReader fr = new FileReader("config.txt");
+            BufferedReader br = new BufferedReader(fr);
+            String test = br.readLine();
+
+            while (test != null) {
+                test = br.readLine();
+                System.out.println(test);
+
+            }
+        } catch (IOException ex) {
+            ex.getMessage();
+        }
+    }
 }
