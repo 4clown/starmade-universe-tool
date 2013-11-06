1a7ebca9d0084dc23532d80e80a293812ba87b7b
wishizerg <>
2013-11-06 15:51:08 +0100

    little fixes

--- eu.4clown.starmade-universe-tool/src/eu/pkg4clown/starmade/universe/tool/Config.java
+++ eu.4clown.starmade-universe-tool/src/eu/pkg4clown/starmade/universe/tool/Config.java
@@ -10,34 +10,34 @@ class Config {
 
     private String test;
     private String command;
-    
+
     public Config(String path) {
-      loadText(path);
-      System.out.println(parseParameter(command));
-     }
-    
-    public String loadText(String path) {
+        loadText(path);
+        System.out.println(parseParameter(command));
+    }
+
+    private String loadText(String path) {
         try {
             FileReader fr = new FileReader(path);
             BufferedReader br = new BufferedReader(fr);
             this.test = br.readLine();
-            
+
             while (test != null) {
                 test = br.readLine();
                 test += test;
             }
-        
+
         } catch (IOException ex) {
             ex.getMessage();
             System.out.println(ex);
         }
         return test;
     }
-    
-    public String parseParameter(String command) {
-       
+
+    private String parseParameter(String command) {
+
         int start = test.indexOf(command) + command.length();
-        int end = test.indexOf("\n",start);
-        return test.substring(start,end);
+        int end = test.indexOf("\n", start);
+        return test.substring(start, end);
     }
 }
